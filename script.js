9be0cc35e9f9cca147a4365286bb0e82002e4a07
metehan <metehansazak@gmail.com>
2022-11-18 11:44:06 +0800

    add minimal JS

--- script.js
+++ script.js
@@ -22,3 +22,39 @@ function operate (a, op, b) {
     return op(a, b);
 }
 console.log(operate(4, add, 7));
+
+const input = document.querySelector('.input');
+const container = document.querySelector('.container');
+const numberBtn = document.querySelectorAll('.numberBtn');
+const operateBtn = document.querySelectorAll('.operateBtn');
+const adder = document.querySelector('#add');
+const equal = document.querySelector('#equals');
+
+let displayValue = '';
+let secondValue = '';
+function numberBtnClicked(e) { 
+    input.textContent += e.target.textContent;
+    displayValue = parseFloat(input.textContent);
+    console.log(displayValue);
+    return displayValue;}
+
+
+function operateBtnClicked(e) {
+    if (e.target === adder) {
+    input.textContent += '+'}
+    else if (e.target === equal) {
+        resultAdd = operate(displayValue, add, displayValue);
+     console.log(resultAdd);
+        input.textContent = resultAdd;
+        return resultAdd;
+    } 
+}
+
+
+operateBtn.forEach(function(button) {
+    button.addEventListener('click', operateBtnClicked);
+});
+
+numberBtn.forEach(function(button) {
+    button.addEventListener('click', numberBtnClicked);
+ });
